f47707a5d38698b8356156dfa51c5e7aaf2c196f
igornerest <igornerest@gmail.com>
2019-08-18 17:50:57 -0300

    Add password change. Fix minor bugs

--- UFABCBarganha/app/src/main/java/br/edu/ufabc/ufabcbarganha/login/ForgotPassActivity.kt
+++ UFABCBarganha/app/src/main/java/br/edu/ufabc/ufabcbarganha/login/ForgotPassActivity.kt
@@ -5,18 +5,22 @@ import android.media.MediaPlayer
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import android.widget.VideoView
 import br.edu.ufabc.ufabcbarganha.App
 import br.edu.ufabc.ufabcbarganha.R
+import com.google.firebase.auth.FirebaseAuth
 
 class ForgotPassActivity : AppCompatActivity() {
 
     lateinit var bgVideo: VideoView
     lateinit var sendEmailButton: Button
     lateinit var emailEditText: EditText
+
+    private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,10 +41,7 @@ class ForgotPassActivity : AppCompatActivity() {
 
     private fun setListeners() {
         sendEmailButton.setOnClickListener{
-            val intent = Intent(this, LoginActivity::class.java)
-            intent.putExtra(App.IS_RETURN, true)
-            Toast.makeText(this, R.string.check_your_email, Toast.LENGTH_LONG).show()
-            startActivity(intent)
+            forgotPassword()
         }
     }
 
@@ -54,4 +55,37 @@ class ForgotPassActivity : AppCompatActivity() {
         super.onResume()
         bgVideo.start()
     }
+
+    private fun forgotPassword() {
+        val email = emailEditText.text.toString()
+
+        val regex = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@((ufabc.edu.br)|(aluno.ufabc.edu.br))".toRegex()
+
+
+        when {
+            email.isEmpty() -> Toast.makeText(this, R.string.empty_mail, Toast.LENGTH_LONG).show()
+
+            !regex.containsMatchIn(email) -> Toast.makeText(this, R.string.valid_email, Toast.LENGTH_LONG).show()
+
+            else -> changePasswordAndReturn()
+        }
+    }
+
+    private fun changePasswordAndReturn() {
+        val email = emailEditText.text.toString()
+
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this, R.string.check_your_email, Toast.LENGTH_LONG).show()
+
+                Intent(this, LoginActivity::class.java).apply {
+                    putExtra(App.IS_RETURN, true)
+                    startActivity(this)
+                }
+
+                finish()
+            }
+        }
+    }
+
 }
